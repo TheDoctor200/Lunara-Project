c93462d246961e9ff21fd52643b8081b0395bbcc
TheDoctor200 <88980346+TheDoctor200@users.noreply.github.com>
2022-10-30 16:19:01 +0100

    Add files via upload

---
+++ Chatbot v.0.01.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import random
+
+zufallsantworten=["Oh, wirklich", "Interessant ...", "Das kann man so sehen", "Ich verstehe ..."]
+
+reaktionsantworten = {"hallo": "aber Hallo", 
+					  "geht": "Was verstehst du darunter?", 
+					  "essen": "Ich habe leider keinen Geschmackssinn :("
+					  }
+                      
+print("Willkommen beim Chatbot")
+print("Worüber würden Sie gerne heute sprechen?")
+print("Zum beenden einfach 'bye' eintippen")
+print("")
+
+nutzereingabe = ""
+while nutzereingabe != "bye":
+    nutzereingabe = ""
+    while nutzereingabe == "":
+        nutzereingabe = input("Ihre Frage/Antwort: ")
+        
+    nutzereingabe = nutzereingabe.lower()
+    nutzerwoerter = nutzereingabe.split()
+    
+    intelligenteAntworten = False
+    for einzelwoerter in nutzerwoerter:
+        if einzelwoerter in reaktionsantworten:
+            print(reaktionsantworten[einzelwoerter])
+            intelligenteAntworten = True
+    if intelligenteAntworten == False:
+        print(random.choice(zufallsantworten))
+        
+    print("")
+
+print("Einen schönen Tag wünsche ich Dir. Bis zum nächsten Mal")
