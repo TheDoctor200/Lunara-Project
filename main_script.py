ab897f305f65aae5217978cdbbc3517f7be957d4
TheDoctor200 <88980346+TheDoctor200@users.noreply.github.com>
2024-02-21 00:53:18 +0100

    Import list of modules used in old Version + flet GUI module

---
+++ main_script.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import random
+import webbrowser
+import speech_recognition as sr
+import flet as ft
+import openai
+from openai import OpenAI 
+import asyncio
+from openai import AsyncOpenAI
